ce34baf8876aa0ca2d19901906572e7ede1bcf0a
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-12-04 09:59:34 +0500

    validation

--- user/serializers.py
+++ user/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.password_validation import validate_password
+
 from shared.utility import check_email_or_phone_num, send_email
 from .models import User, UserConfirmation, VIA_PHONE, VIA_EMAIL, NEW, CODE_VERIFIED, DONE, PHOTO_STEP
 from rest_framework import exceptions
@@ -85,6 +87,71 @@ class UserSignUpSerializer(serializers.ModelSerializer):
 
 class VerifyCodeSerializer(serializers.Serializer):       # noqa
     code = serializers.CharField(max_length=4, write_only=True)
+
+
+class ChangeUserInformation(serializers.Serializer):      # noqa
+    first_name = serializers.CharField(max_length=15, required=True, write_only=True)
+    last_name = serializers.CharField(max_length=15, required=True, write_only=True)
+    username = serializers.CharField(max_length=15, required=True, write_only=True)
+    password = serializers.CharField(max_length=255, required=True, write_only=True)
+    confirm_password = serializers.CharField(max_length=255, required=True, write_only=True)
+
+    def validate(self, attrs):
+        password = attrs.get('password', None)
+        confirm_password = attrs.get('confirm_password', None)
+        if password != confirm_password:
+            data = {
+                'success': False,
+                'message': 'Parolingiz va tasdiqlash parolingiz bir biriga teng emas!'
+            }
+            raise ValidationError(data)
+        if password:
+            validate_password(password)
+            validate_password(confirm_password)
+
+        return attrs
+
+    def validate_username(self, username):  # noqa
+        if len(username) < 5 or len(username) > 35:
+            data = {
+                'success': False,
+                'message': "Username 5 dan kam 15 dan ko'p bolmasligi kerak!"
+            }
+            raise ValidationError(data)
+
+        return username
+
+    def validate_first_name(self, first_name):  # noqa
+        if len(first_name) < 5 or len(first_name) > 35:
+            data = {
+                'success': False,
+                'message': "Ismingiz 5 dan kam 15 dan ko'p bolmasligi kerak!"
+            }
+            raise ValidationError(data)
+
+        if first_name.isdigit():
+            data = {
+                'success': False,
+                'message': "Ismingiz faqat harflardan tashkil topgan bolishi kerak"
+            }
+
+        return first_name
+
+    def validate_last_name(self, last_name):  # noqa
+        if len(last_name) < 5 or len(last_name) > 35:
+            data = {
+                'success': False,
+                'message': "Familyangiz 5 dan kam 15 dan ko'p bolmasligi kerak!"
+            }
+            raise ValidationError(data)
+        if last_name.isdigit():
+            data = {
+                'success': False,
+                'message': "Familyangiz faqat harflardan tashkil topgan bolishi kerak"
+            }
+
+        return last_name
+
 
 
 
