74e5c73d8cee5a5f304114d55e691ba53f0475bd
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-12-06 17:27:22 +0500

    create PostLikeSerializer, CommentLikeSerializer

--- post/serializers.py
+++ post/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from post.models import Post, PostLike
+from post.models import Post, PostLike, PostComment, CommentLike
 from user.models import User
 
 
@@ -38,5 +38,52 @@ class PostSerializer(serializers.ModelSerializer):
             except PostLike.DoesNotExist:
                 return False
         return False
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    author = UserSerializer(read_only=True)
+    replies = serializers.SerializerMethodField('get_replies')
+    likes_count = serializers.SerializerMethodField('get_likes_count')
+    me_liked = serializers.SerializerMethodField('get_me_liked')
+
+    class Meta:
+        model = PostComment
+        fields = ('id', 'author', 'comment', 'parent', 'created_time', 'replies', 'likes_count', 'me_liked')
+
+    def get_replies(self, obj):
+        if obj.chiled.exists():
+            serializers = self.__class__(obj.chiled.all(), many=True, context=self.context)  # noqa
+            return serializers.data
+        else:
+            return None
+
+    def get_me_liked(self, obj):
+        user = self.context.get('request').user
+        if user.is_authenticated:
+            return obj.likes.filter(author=user).exists()
+        else:
+            return False
+
+    def get_likes_count(self, obj):   # noqa
+        return obj.likes.count()
+
+
+class CommentLikeSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = CommentLike
+        fields = ('id', 'author')
+
+
+class PostLikeSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = PostLike
+        fields = ('id', 'author', 'post')
 
 
