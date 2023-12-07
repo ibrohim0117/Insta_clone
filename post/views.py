a34f2603589feed7122066f0f890a0fd6d551a6e
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-12-07 12:01:34 +0500

    add CustomPagination

--- post/views.py
+++ post/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from shared.custom_pagination import CustomPagination
 from .models import Post, PostLike, PostComment
 from .serializers import PostSerializer, PostLikeSerializer, CommentSerializer, CommentLikeSerializer
 from rest_framework import generics, permissions
@@ -8,4 +10,5 @@ class PostListApiView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+    pagination_class = CustomPagination
 
