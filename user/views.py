d09a46a7003993aa600a6ad756c29a338738696a
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-12-07 11:18:26 +0500

    .

--- user/views.py
+++ user/views.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from django.core.exceptions import ObjectDoesNotExist
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import permissions, status, generics
